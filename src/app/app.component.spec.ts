39576f6fc8683ff865cdc79c8334e71e38226291
Keith Ehlis <keith@ehlis.com>
2017-11-19 19:58:45 -0600

    String Calculator TDD Kata

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,58 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    component.value = '';
+
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  it('should return 0 for an empty string', async(() => {
+    expect(component.sum).toBe(0);
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it('should return a single number correctly', async(() => {
+    component.add('1');
+    expect(component.sum).toBe(1);
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+  it('should calculate the sum of two numbers separated by a comma correctly', async(() => {
+    component.add('1, 2');
+    expect(component.sum).toBe(3);
   }));
+
+  it('should calculate the sum of three numbers separated by a comma correctly', async(() => {
+    component.add('1, 2, 3');
+    expect(component.sum).toBe(6);
+  }));
+
+  it('should support various delimeters of: , ; \n space |', async(() => {
+    component.add('1\n2;3|4 5');
+    expect(component.sum).toBe(15);
+  }));
+
+  it('should throw an error for negative numbers', async(() => {
+    component.add('1, 2, -7');
+    expect(component.error).toBe('negatives not allowed');
+  }));
+
+  it('should throw an error for negative numbers and list if multiple negative numbers', async(() => {
+    component.add('1, -2, -7, 5');
+    expect(component.error).toBe('negatives not allowed : -2,-7');
+  }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,84 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>{{ title }}</h1>
+
+      <textarea placeholder="Enter number(s) - separated by: ; , | space or new line or literally \n" #numbers></textarea>
+
+      <button type="button" (click)="add(numbers.value)">Get Sum</button>
+
+      <div *ngIf="!error" class="sum">
+        Sum: {{ sum }}
+      </div>
+
+      <div *ngIf="error" class="error">
+        {{ error }}
+      </div>
+
+    </div>
+  `,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'String Calculator';
+  error;
+  value;
+  sum = 0;
+
+  /**
+   * Calculate sum of string
+   * @param event
+   */
+  add(event: string) {
+
+    this.error = '';
+
+    let nums: any[] = [];
+    let errors: any[] = [];
+
+    // display 0 for empty stirng
+    if (event.length === 0) {
+
+      this.sum = 0;
+
+    } else {
+
+      // put string into array
+      nums = event.split(/[\\n,;\s|]+/);
+
+      // trim each number in array
+      nums = nums.map(num => num.trim());
+
+      // remove empty string elements from array
+      nums = nums.filter(num => num !== '');
+
+      // filter out numbers containing negative and put them in own array
+      errors = nums.filter(num => num < 0);
+
+      // check for negative numbers, if so, throw error
+      if (errors.length) {
+
+        // list each negative if multiple
+        if (errors.length > 1) {
+          this.error = 'negatives not allowed : ' + errors.toString();
+        } else {
+          this.error = 'negatives not allowed';
+        }
+
+      }
+
+      // if no errors, calculate sum
+      const result = nums.map(num => parseInt(num, 10))
+      .reduce((a, b) => a + b, 0);
+
+      // display sum
+      this.sum = result;
+
+    }
+
+  }
+
 }
